05cedd6c7f11d60908e554faf0aa3d73c5e0fcf7
Chike T. Abuah <abuahchu@grinnell.edu>
2012-12-22 03:42:44 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaurenFlynn::Application.config.secret_token = 'a0437a1ab15c0f2815c9645b2d6030aef975e845ee308280f152691d2c1a980b20bab2da20b8fa7cb7dd1da0a5500955b3a43a8943e628655abd08fe7d2d514f'
